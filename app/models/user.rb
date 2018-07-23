082d7734623a9c80e5b2301ac16ea50bf4da78ea
Carmen Shing <carmenshing@gmail.com>
2018-07-23 22:30:24 +0000

    add validation for password minimum length, and downcase email before validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,18 @@
 class User < ActiveRecord::Base
 
     has_secure_password
+    before_validation :downcase_email
 
     validates :first_name, presence: true
     validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true
     validates :password_digest, presence: true
+    validates :password, :password_confirmation, length: { minimum: 8 }
+
+    private
+
+    def downcase_email
+      self.email = email.downcase if email.present?
+    end
 
 end
