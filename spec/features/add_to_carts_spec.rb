3f8c390e907b0406c037c22224faf9c5b5cb1a17
Carmen Shing <carmenshing@gmail.com>
2018-07-24 21:59:40 +0000

    add testing for add to cart, cart updates from 0 to 1

---
+++ spec/features/add_to_carts_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.feature "Add to cart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "cart updates to 1" do
+    # ACT
+    visit root_path
+
+    first('article.product').find_link('Add').click
+
+    # DEBUG
+    save_screenshot('addToCart.png')
+
+    # VERIFY
+    expect(page).to have_content 'My Cart (1)'
+  end
+end
