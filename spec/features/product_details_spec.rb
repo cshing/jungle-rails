95ba2e1b36eeeb02be167643f81ce2b4a5608680
Carmen Shing <carmenshing@gmail.com>
2018-07-24 21:22:01 +0000

    add testing for displaying product details page

---
+++ spec/features/product_details_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor navigates to product details page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "They see the product details of a product" do
+    # ACT
+    visit root_path
+
+    first('article.product').find_link('Details').click
+
+    visit product_path(1)
+
+    # DEBUG
+    save_screenshot('product-detail.png')
+
+    # VERIFY
+    expect(page).to have_css 'article.product-detail'
+  end
+end
